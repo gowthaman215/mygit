db4ed44ddb34b3830eaddd1dde23aa23c4d2b1c8
Gowthaman Palanisamy <gowthaman215@gmail.com>
2020-10-17 14:38:11 +0530

    Eclipse Project files added which is using gnu c compiler

---
+++ Tutorial/C/Eclipse Project/HelloWorld/src/HelloWorld.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : HelloWorld.c
+ Author      : 
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+	puts("!!!Hello World Welcome to C programing using eclipse!!!"); /* prints !!!Hello World!!! */
+	return EXIT_SUCCESS;
+}
